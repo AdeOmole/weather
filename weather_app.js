25e1417af5c8538dc42534bb1222fc610476fced
AdeOmole <66742928+AdeOmole@users.noreply.github.com>
2020-06-10 18:48:00 +0100

    Update weather_app.js

--- weather_app.js
+++ weather_app.js
@@ -3,7 +3,7 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
-const time = document.querySelector('.time');
+const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 const forecast = new Forecast();
 
