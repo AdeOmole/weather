79b31a35f0dde1e4c398f7993906770bad2c7128
AdeOmole <66742928+AdeOmole@users.noreply.github.com>
2020-06-10 17:42:31 +0100

    Add files via upload

---
+++ weather_app.js
@@ -0,0 +1,68 @@
+// DOM Manipulation
+
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+const updateUI = (data) => {
+    // destructure properties
+    const {cityDets: cityDetails, weather } = data;
+
+    // update template details
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+    // update the night and day svg images
+    const iconSource = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSource);
+
+    let timerSource = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+    // let timerSource = null;
+    // if(weather.IsDayTime){
+    //     timerSource = 'img/day.svg';
+    // }else{
+    //     timerSource = 'img/night.svg';
+    // };
+    time.setAttribute('src', timerSource);
+
+
+    // remove the d-none if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    };
+};
+
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update UI with new city
+    forecast.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+    // set local storage
+    localStorage.setItem('city', city);
+
+});
+
+if(localStorage.getItem('city') !== null){
+    const lastUsed = localStorage.getItem('city');
+    forecast.updateCity(lastUsed)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
